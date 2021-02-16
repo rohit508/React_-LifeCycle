1d8b570f1d5960b47d7b93584c59b4d1976ede48
rohit508 <lachmanrohit282@gmail.com>
2021-02-17 01:31:15 +0500

    life cycle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+    // react lifecycle methods
+    // =============== constructor ==========
+
+    constructor() {
+        super()
+        this.state = {
+            count: 0,
+            Data: []
+        }
+        // console.log("construtor");
+    }
+
+    // =============== getDerivedStateFromProps ==========
+
+    // static getDerivedStateFromProps() {
+    //     // console.log("getDerivedStateFromProps")
+    //     return {
+    //         count: 10,
+
+
+    //     }
+
+
+    // }
+
+    // ================= componentDidMount ================
+    // this  methods use for call Api 
+
+    // componentDidMount() {
+    //     console.log("componentDidMount")
+
+    //     fetch('https://jsonplaceholder.typicode.com/todos')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         this.setState({
+    //             Data : data
+    //         })
+    //     })
+    //     .catch( arr => console.log(arr))
+    // }
+
+
+
+
+    // ================= render ================
+    //  static getDerivedStateFromProps(props, state) {
+    //     console.log("getDerivedStateFromProps" ,state)
+    //     return  null
+
+
+    //     }
+
+    // shouldComponentUpdate(){
+    //     return false
+    // }
+
+    componentWillUnmount() {
+        console.log("component End")
+    }
+    render() {
+        // console.log("render..")
+        // console.log("data==>",this.state.Data)
+
+        return (
+            <div style={{ textAlign: "center" }}>
+
+
+                <h1>Life cycle</h1>
+
+                {this.state.count < 5 && <h1>{this.state.count}</h1>}
+                <button onClick={() => { this.setState({ count: this.state.count + 1 }) }}>increament</button>
+                <button onClick={() => { this.setState({ count: this.state.count - 1 }) }}>decreament</button>
+
+
+                {/*  Api call  */}
+
+                {/* {this.state.Data.map((v,i) => {
+                    return <h5>{v.title}</h5>
+                    
+                })} */}
+            </div>
+        )
+    }
 }
 
-export default App;
+
+// Mounting: 
+
+// constructor()
+// getDerivedStateFromProps()
+// render()
+// componentDidMount()
+
+
+
+
+
+
+// Updating : 
+
+// getDerivedStateFromProps()
+// shouldComponentUpdate()
+// render()
+// getSnapshotBeforeUpdate() props and state
+
+// componentDidUpdate()
+
+
+
+
+
+// Unmounting :
+
+// componentWillUnmount()
